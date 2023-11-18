84af1d5bd729994fd91971a9e22b285ee1d47458
unknown <loc842211@gmail.com>
2023-11-18 10:35:40 +0700

    dan layout bootstrap 5

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,29 @@
+import './Login.scss'
 const Login = (props) => {
     return (
-        <div className="login-container">
-            Login
-            <button>Test</button>
+        <div className="login-container mt-3">
+            <div className="container">
+                <div className="row">
+                    <div className="content-left col-7">
+                        <div className="brand">
+                            Tran Quoc Loc
+                        </div>
+                        <div className="detail">
+                            Tran Quoc Loc helps you connect and share with the people in your life
+                        </div>
+                    </div>
+                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                        <input type="text" className='form-control' placeholder='Email address or phone number' />
+                        <input type="password" className='form-control' placeholder='passwowd' />
+                        <button className='btn btn-primary'>Login</button>
+                        <span className='text-center'>Forgot your password</span>
+                        <hr />
+                        <div className='text-center'>
+                            <button className='btn btn-success'>Create new account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
