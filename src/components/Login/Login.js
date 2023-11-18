22e6eda03c714941ac23054f9551bc12b995be7e
unknown <loc842211@gmail.com>
2023-11-18 16:26:22 +0700

    tao form cho mobile/laptop

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,8 +3,8 @@ const Login = (props) => {
     return (
         <div className="login-container mt-3">
             <div className="container">
-                <div className="row">
-                    <div className="content-left col-7">
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-12 d-none col-sm-7 d-sm-block">
                         <div className="brand">
                             Tran Quoc Loc
                         </div>
@@ -12,11 +12,16 @@ const Login = (props) => {
                             Tran Quoc Loc helps you connect and share with the people in your life
                         </div>
                     </div>
-                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                    <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+                        <div className="brand d-sm-none">
+                            Tran Quoc Loc
+                        </div>
                         <input type="text" className='form-control' placeholder='Email address or phone number' />
                         <input type="password" className='form-control' placeholder='passwowd' />
                         <button className='btn btn-primary'>Login</button>
-                        <span className='text-center'>Forgot your password</span>
+                        <span className='text-center'>
+                            <a className='forgot-password' href='#'>Forgot your password</a>
+                        </span>
                         <hr />
                         <div className='text-center'>
                             <button className='btn btn-success'>Create new account</button>
